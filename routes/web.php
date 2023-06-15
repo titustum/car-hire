abe0dade84c8ef0aa14053cd537d7c56ab60382a
nyatado98 <danndong080@gmail.com>
2023-06-15 19:03:04 +0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarsController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $cars = DB::select("SELECT * FROM cars WHERE car_type = '4X4'");
+        return view('index',compact('cars'));
+    // return view('index');
 });
 Route::get('login',function(){
     return view('login');
